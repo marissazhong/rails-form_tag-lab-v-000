fd93fcd1328037580726122b8c2b1038a27fe078
Marissa Zhong <mwz2101@columbia.edu>
2018-07-24 21:46:52 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -10,10 +10,10 @@ class StudentsController < ApplicationController
 
   def new
   end
-  
+
   def create
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to posts_path
+    redirect_to students_path
   end
 
 end
